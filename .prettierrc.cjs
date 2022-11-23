2e168ba5142f1087e11b0ba1c78f309370721fd2
Tyler Rick <tyler@tylerrick.com>
2022-11-22 20:20:47 -0800

    Deps

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 120,
+
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+
+  //plugins: ['prettier-plugin-svelte'],
+  svelteAllowShorthand: true,
+  svelteSortOrder: 'options-markup-scripts-styles',
+  svelteIndentScriptAndStyle: true,
+  svelteStrictMode: false,
+}
